9f987a98be2f0f1e93714093711d1b560ff1d2c7
Natali <natalagurban7@gmail.com>
2023-03-05 12:29:22 +0300

    Решение задачи4

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное
+// из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+Console.Clear();
+Console.WriteLine("Введите первое число: ");
+int A=int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int B=int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите третье число: ");
+int C=int.Parse(Console.ReadLine()!);
+int max=A;
+if (max<B) max=B;
+if (max<C) max=C;
+Console.WriteLine($"Максимальное число: {max}");
